06b221d908dcb967217600a3cf9e59586c37b302
Jeremy Mack Wright <wrightjmf@gmail.com>
2016-05-23 22:17:39 -0400

    Initial commit

---
+++ eggbeater-antenna/eggbeater_antenna.py
@@ -0,0 +1,58 @@
+import math
+import FreeCAD
+import cadquery as cq
+from Helpers import show
+
+# Driving parameter
+freq = 145.8  # MHz
+aerial_material = 'cu'  # Aerial material (cu, rg-62, rg-213)
+
+# DO NOT CHANGE ANYTHING FROM HERE DOWN #
+
+# Constants
+C = 299.792  # m/sec
+
+# Derived variables
+vac_wl = C / freq        # wavelength in a vacuum (m)
+cu_wl = vac_wl * 0.93          # wavelength in copper (m)
+rg62_wl = vac_wl * 0.83        # wavelength in RG-62 (m)
+rg213_wl = vac_wl * 0.66       # wavelength in RG-213 (m)
+
+# Our material affects the wave velocity and thus the dimensions
+wavelength = None
+if aerial_material == "cu":
+    wavelength = cu_wl
+elif aerial_material == "rg-62":
+    wavelength = rg62_wl
+elif aerial_material == "rg-213":
+    wavelength = rg213_wl
+else:
+    wavelength = cu_wl
+
+# Relationships
+aerial_len = (1005 / (C / wavelength)) * 0.3048  # Aerial length (m)
+aerial_dia = aerial_len / math.pi  # The resulting diameter of the aerials
+gp_dia = 2.0 * aerial_dia        # Ground plane dia = 2x aerial dia
+gp_aerial_centers = 0.125 * wavelength     # Distance between center of aerials and ground plane (m)
+base_dia = gp_dia / 2.0                    # The diameter of the antenna base
+
+# Figure out the body tube size
+mast_dia = None  # Body tube diameter (m)
+if freq < 120.0:
+    mast_dia = 0.0508  # 2" dia
+elif freq > 120.0 and freq < 500:
+    mast_dia = 0.0381  # 1-1/2" dia
+else:
+    mast_dia = 0.01905  # 3/4" dia
+
+# Figure out the points for our loop to go through
+p1 = (mast_dia / 2.0, 0.0)   # Loop connection point with mast #1
+p2 = (0, aerial_dia)         # The top of the aerial loop
+p3 = (-mast_dia / 2.0, 0.0)  # Loop connection point with mast #2
+aerial1 = cq.Workplane('XY').center(p1[0], p1[1]).threePointArc(p2, p3)
+
+# Output some of the dimensions that we have calculated
+FreeCAD.Console.PrintMessage("Aerial length: " + str(aerial_len) + " m \r\n")
+FreeCAD.Console.PrintMessage("Aerial length: " + str(aerial_dia) + " m \r\n")
+
+show(aerial1)
